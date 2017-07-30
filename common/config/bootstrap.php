c1483eadbb0c593f962f2de166fdbabfdfc6cd53
Antonov <thorder83@gmail.com>
2017-07-31 02:03:43 +0700

    init commit

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -4,7 +4,6 @@ Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('@console', dirname(dirname(__DIR__)) . '/console');
 Yii::setAlias('@api', dirname(dirname(__DIR__)) . '/api');
-
 Yii::$container->setSingleton(\Dotenv\Dotenv::class, function () {
     $env = new Dotenv\Dotenv(dirname(dirname(__DIR__)));
     $env->load();
